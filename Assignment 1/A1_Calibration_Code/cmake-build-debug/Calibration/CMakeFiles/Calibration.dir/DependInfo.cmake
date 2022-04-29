dd70391bf15007fac9d8d425160312b9d478f590
mvancapel <m.a.vancapel@student.tudelft.nl>
2022-04-29 14:17:11 +0200

    Added calibration assignment
    Added calibration assignment

---
+++ Assignment 1/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/Calibration/calibration.cpp" "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/calibration.cpp.obj"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/Calibration/calibration_method.cpp" "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/calibration_method.cpp.obj"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/Calibration/main.cpp" "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/main.cpp.obj"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/Calibration/matrix_algo.cpp" "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/Calibration/CMakeFiles/Calibration.dir/matrix_algo.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_NO_GLU"
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "."
+  "../3rd_party/glog/.."
+  "../3rd_party/glew/include"
+  "../3rd_party/glfw/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/easy3d/core/CMakeFiles/easy3d_core.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/easy3d/viewer/CMakeFiles/easy3d_viewer.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/3rd_party/glew/CMakeFiles/3rd_glew.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/3rd_party/glfw/src/CMakeFiles/3rd_glfw.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/easy3d/util/CMakeFiles/easy3d_util.dir/DependInfo.cmake"
+  "C:/Users/mavc/Documents/Geomatics/Photogrammetry and 3D computer vision/Assignment 1/A1_Calibration_Code/cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
